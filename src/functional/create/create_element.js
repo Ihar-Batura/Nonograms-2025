c1887544787e1fda2c85572dcef84c94aa726e8b
Ihar-Batura <A17331582@gmail.com>
2025-01-21 16:42:46 +0300

    feat: add function createElement

---
+++ src/functional/create/create_element.js
@@ -0,0 +1,22 @@
+function createElement(options) {
+  const { tag = 'div', classes = [], id = '', text = '', parent } = options;
+
+  const element = document.createElement(tag);
+
+  if (classes.length > 0) {
+    element.classList.add(...classes);
+  }
+  if (id) {
+    element.id = id;
+  }
+  if (text) {
+    element.textContent = text;
+  }
+  if (parent != null) {
+    parent.append(element);
+  }
+
+  return element;
+}
+
+export default createElement;
