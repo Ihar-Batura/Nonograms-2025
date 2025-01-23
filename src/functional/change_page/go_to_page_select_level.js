670b10a6a9a8f83d5a0168e192d03f0efe705c31
Ihar-Batura <A17331582@gmail.com>
2025-01-23 13:49:29 +0300

    feat: add removeClassFromAllElements when click button select level

--- src/functional/change_page/go_to_page_select_level.js
+++ src/functional/change_page/go_to_page_select_level.js
@@ -1,7 +1,11 @@
 import cleanMainContent from '../clean/clean_main_content';
 import createSectionSelectLevel from '../../layout/select_level';
+import removeClassFromAllElements from '../clean/remove_class_from_all_elements';
 
-function goToPageSelectLevel() {
+function goToPageSelectLevel(event) {
+  const element = event.target;
+  removeClassFromAllElements('nav-btn', 'active');
+  element.classList.add('active');
   cleanMainContent();
   const mainContentContainer = document.querySelector('.main-content');
   const selectLevelContent = createSectionSelectLevel();
