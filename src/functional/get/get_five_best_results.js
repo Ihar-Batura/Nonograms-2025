7147860f13622cd0e5239484e88083ed6c106ace
Ihar-Batura <A17331582@gmail.com>
2025-01-24 16:14:25 +0300

    feat: add function getFiveBestResultsForTable

---
+++ src/functional/get/get_five_best_results.js
@@ -0,0 +1,27 @@
+function getFiveBestResultsForTable() {
+  const arrBestResults = JSON.parse(localStorage.getItem('NGResultsList'));
+  if (arrBestResults !== null) {
+    let arrSortedResults = arrBestResults.sort(function (a, b) {
+      if (a.result > b.result) {
+        return 1;
+      }
+      if (a.result < b.result) {
+        return -1;
+      }
+      return 0;
+    });
+
+    const tableRowsList = document.querySelectorAll('.table-body__row');
+    let count = 0;
+    tableRowsList.forEach((row) => {
+      if (arrSortedResults !== null && arrSortedResults[count] !== undefined) {
+        row.children[0].textContent = arrSortedResults[count].name;
+        row.children[1].textContent = arrSortedResults[count].difficulty;
+        row.children[2].textContent = arrSortedResults[count].result;
+      }
+      count += 1;
+    });
+  }
+}
+
+export default getFiveBestResultsForTable;
