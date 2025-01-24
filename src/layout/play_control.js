d568d788f2f90c85343e70277fd3ab213b3c6d5c
Ihar-Batura <A17331582@gmail.com>
2025-01-24 16:15:10 +0300

    feat: add onclick to navigation buttons

--- src/layout/play_control.js
+++ src/layout/play_control.js
@@ -1,6 +1,10 @@
 import createElement from '../functional/create/create_element';
 import createTimer from '../components/timer';
 import createButton from '../functional/create/create_button';
+import showSolution from '../functional/game/show_solution';
+import resetGame from '../functional/game/reset_game';
+import saveGame from '../functional/game/save_game';
+import continueLastGame from '../functional/game/continue_last_game';
 
 function createPlayControl() {
   const controlContainer = createElement({
@@ -14,21 +18,25 @@ function createPlayControl() {
   createButton({
     classes: ['btn', 'play-btn', 'play-btn__save-game'],
     text: 'Save game',
+    onClick: () => saveGame(),
     parent: controlContainer,
   });
   createButton({
     classes: ['btn', 'play-btn', 'play-btn__reset-game'],
     text: 'Reset game',
+    onClick: () => resetGame(),
     parent: controlContainer,
   });
   createButton({
     classes: ['btn', 'play-btn', 'play-btn__show-solution'],
     text: 'Show solution',
+    onClick: () => showSolution(),
     parent: controlContainer,
   });
   createButton({
     classes: ['btn', 'play-btn', 'play-btn__continue-game'],
     text: 'Continue last game',
+    onClick: () => continueLastGame(),
     parent: controlContainer,
   });
 
