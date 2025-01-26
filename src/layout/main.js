16a3767d4f911ac91d9afd60dadb3bfe05ac6d06
Ihar-Batura <A17331582@gmail.com>
2025-01-26 13:48:34 +0300

    feat: add start game to start page

--- src/layout/main.js
+++ src/layout/main.js
@@ -1,6 +1,5 @@
 import createElement from '../functional/create/create_element';
 import createNavigation from '../components/navigation';
-import createSectionSelectLevel from '../layout/select_level';
 
 function createMain() {
   const main = createElement({ tag: 'main', classes: ['main'] });
@@ -13,9 +12,7 @@ function createMain() {
 
   const navigation = createNavigation();
   const mainContent = createElement({ tag: 'div', classes: ['main-content'] });
-  const sectionSelectLevel = createSectionSelectLevel();
 
-  mainContent.append(sectionSelectLevel);
   wrapper.append(navigation, mainContent);
 
   return main;
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import './sass/style.scss';
 import createHeader from './layout/header';
 import createMain from './layout/main';
 import createFooter from './layout/footer';
+import getRandomNumber from './functional/get/get_random_number';
+import goToPlayPage from './functional/change_page/go_to_play_page';
 
 const body = document.querySelector('body');
 const header = createHeader();
@@ -9,3 +11,6 @@ const main = createMain();
 const footer = createFooter();
 
 body.append(header, main, footer);
+
+const startRound = getRandomNumber(1, 5);
+goToPlayPage(startRound);
