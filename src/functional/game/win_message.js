4b1cf38a6147bc203c97536a4a22c92acfe0cae8
Ihar-Batura <A17331582@gmail.com>
2025-01-27 16:06:27 +0300

    feat: add function createWinMessage

---
+++ src/functional/game/win_message.js
@@ -0,0 +1,39 @@
+import createElement from '../create/create_element';
+import createButton from '../create/create_button';
+import goToPlayPage from '../change_page/go_to_play_page';
+
+function createWinMessage(id, name, time) {
+  const mainContentContainer = document.querySelector('.main-content');
+
+  const winMessageContainer = createElement({
+    tag: 'div',
+    classes: ['win-message__container'],
+    parent: mainContentContainer,
+  });
+
+  createElement({
+    tag: 'h2',
+    classes: ['win-message__container-text'],
+    text: `"Great! You have solved the nonogram in ${time} seconds!"`,
+    parent: winMessageContainer,
+  });
+
+  createElement({
+    tag: 'div',
+    classes: [
+      'win-message__container-picture',
+      'level-card__img',
+      `level-card__img-${name}`,
+    ],
+    parent: winMessageContainer,
+  });
+
+  createButton({
+    classes: ['btn', 'play-btn', 'play-btn__play-again'],
+    text: 'Play again',
+    onClick: () => goToPlayPage(id),
+    parent: winMessageContainer,
+  });
+}
+
+export default createWinMessage;
