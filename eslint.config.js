1bd87aceed89f8a26242e0caaf1818a1c44b62dd
Ihar-Batura <A17331582@gmail.com>
2025-01-31 12:48:35 +0300

    chore: fix file eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ["node_modules", "dist"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-
+  { ignores: ['node_modules', 'dist'] },
   {
+    extends: [pluginJs.configs.recommended, eslintPluginPrettier],
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.browser },
     linterOptions: {
       noInlineConfig: true,
     },
